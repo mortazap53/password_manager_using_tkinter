de7c7e4a26621ce305f7c67d60196f3f93c95bc2
R1stAMOR <mortazap53@gmail.com>
2026-03-07 11:25:30 +0430

    in user interface making the website label and entry

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ window.configure(padx=50, pady=50, bg="white")
 canvas = Canvas(window, width=200, height=200, bg="white", highlightthickness=0)
 image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=image)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:", bg="white")
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=46, bg="white")
+website_entry.focus_set()
+website_entry.grid(row=1, column=1, columnspan=2)
 
 window.mainloop()
